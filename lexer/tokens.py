a173e9ac54c972d58881280593c0e45ac18717e2
Jorge <jorgejapao00@gmail.com>
2025-10-31 12:02:52 -0300

    comments added to tokens list

--- lexer/tokens.py
+++ lexer/tokens.py
@@ -1,17 +1,26 @@
 stringMatch = r""""[^"]*"|'[^']*'"""
 
 TOKENS = [
+    ## Basic Types ##
+    
     ("NUMBER", r"\d+"),
     ("STRING", stringMatch),
     ("IDENT", r"[a-zA-Z_][a-zA-Z0-9_]*"),
+
+    ## Basic Signals ##
+
     ("PLUS", r"\+"),
     ("MINUS", r"-"),
     ("TIMES", r"\*"),
     ("DIVIDE", r"/"),
+
     ("LPAREN", r"\("),
     ("RPAREN", r"\)"),
+
+    ## Identifiers ##
+
     ("METHOD", r"\@method"),
-    ("CLASS", r"\@[[:upper:]][^.\s]*")
+    ("CLASS", r"\@[[:upper:]][^.\s]*"),
     ("CLASS_CALLED", r"\.[[:lower:]]*")
 ]
 
