0be5d41a87a4b05d0921a996d4b233e31cdff6e4
Jorge <jorgejapao00@gmail.com>
2025-11-01 15:53:19 -0300

    tokenizer changed to use lark

--- lexer/tokenizer.py
+++ lexer/tokenizer.py
@@ -1,83 +1,11 @@
-class Lexer():
-    symbol_map = {
-        "+": "PLUS",
-        "-": "MINUS",
-        "*": "TIMES",
-        "/": "DIVIDE",
-        "(": "LPAREN",
-        ")":"RPAREN",
-    }
-    state_map = {
-        "DEFAULT"
-        "NUMBER"
-        "STRING"
-    }
+from lark import *
 
-    state = "DEFAULT"
-
-    def changeState(arg):
-        Lexer.state = arg
-
-    def resetState():
-        Lexer.state = "DEFAULT"
-
-
-    buffer = ''
-
-    def resetBuffer():
-        Lexer.buffer = ''
-
-    tokens = []
-
-    def readNumber(current):
-        if Lexer.state == "NUMBER":
-            Lexer.buffer += current
-        else:
-            Lexer.tokens.append(("NUMBER", Lexer.buffer))
-            Lexer.resetBuffer()
-            Lexer.resetState()
-    
-    def readString(current):
-        if Lexer.state == "STRING":
-            Lexer.buffer += current
-            print("Buffer: " + Lexer.buffer)
-    
-
-
-    def tokenize(font):
-        quote = ''
-        quoteList = ['"', "'"]
-        for char in font:
-            if Lexer.state == "STRING":
-                if char == quote:
-                    Lexer.tokens.append(("STRING", Lexer.buffer))
-                    Lexer.resetBuffer()
-                    Lexer.resetState()
-                    quote = ''
-                else:
-                    Lexer.readString(char)
-            elif char in quoteList:
-                Lexer.changeState("STRING")
-                quote = char
-            elif char == " " and Lexer.state != "DEFAULT":
-                Lexer.tokens.append((Lexer.state, Lexer.buffer))
-                Lexer.resetBuffer()
-                Lexer.resetState()
-            elif char in Lexer.symbol_map:
-                Lexer.tokens.append((Lexer.symbol_map[char], char))
-            elif char.isdigit():
-                Lexer.changeState("NUMBER")
-                Lexer.readNumber(char)
-            elif char in quoteList or Lexer.state == "STRING":
-                if char in quoteList:
-                    char = quote
-                Lexer.changeState("STRING")
-                Lexer.readString(char)
-    
-        return Lexer.tokens
-    
-
+grammar = open(r'lexer\grammar.lark').read()
+Praser = Lark(grammar, parser='earley', propagate_positions=True)
 
 
 if __name__ == "__main__":
-    print(Lexer.tokenize('3 + 69 + 9 - 8 * 65 + "aa++--//**aa" + 5 "teste MALUCO" + "olá, mundo"'))
+    code = open(r'examples\exemplo2.at').read()
+    tree = Praser.parse(code)
+    print(tree.pretty())
+
