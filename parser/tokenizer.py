d05f93dc397fa02588f1735b5dacdb79d4062ad4
Jorge <jorgejapao00@gmail.com>
2025-11-01 16:16:32 -0300

    function to build tree moved to a class

--- parser/tokenizer.py
+++ parser/tokenizer.py
@@ -1,11 +1,17 @@
 from lark import *
 
-grammar = open(r'lexer\grammar.lark').read()
-Praser = Lark(grammar, parser='earley', propagate_positions=True)
+class AtomiqParser():
+    grammar = open(r'lexer\grammar.lark').read()
+    praser = Lark(grammar, parser='earley', propagate_positions=True)
+
+    @staticmethod
+    def build(code):
+        tree = Tree.praser.parse(code)
+        return tree
+
 
 
 if __name__ == "__main__":
     code = open(r'examples\exemplo2.at').read()
-    tree = Praser.parse(code)
-    print(tree.pretty())
+    print(AtomiqParser.build(code))
 
