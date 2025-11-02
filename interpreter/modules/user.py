5db3d58144b3132516a6be078adf2a94abb63b29
Jorge <jorgejapao00@gmail.com>
2025-11-02 20:32:30 -0300

    clear terminal funcion added

--- interpreter/modules/user.py
+++ interpreter/modules/user.py
@@ -1,4 +1,7 @@
+import os
+
 class User():
+    ## @User.show:string:
     @staticmethod
     def show(*args):
         if len(args) > 1:
@@ -7,3 +10,7 @@ class User():
             raise Exception("Execution Error: @User.show can not be empty.")
         if len(args) == 1:
             print(args[0])
+
+    @staticmethod
+    def clear():
+        os.system('cls')
