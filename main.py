fca93c9989e318d2497df6885fb6102d8134aaf1
Jorge <jorgejapao00@gmail.com>
2025-11-03 18:08:54 -0300

    main.py with execution time added

---
+++ main.py
@@ -0,0 +1,35 @@
+from parser.tokenizer import AtomiqParser
+from interpreter.interpreter import AtomiqInterpreter
+import time
+import os
+
+
+
+def runAtCode(path: str):
+    startTime = time.perf_counter()
+
+    codePath = r'examples\exemplo2.at'
+    code = open(codePath).read()
+    tree = AtomiqParser.build(code)
+    interpreter = AtomiqInterpreter()
+
+    os.system('cls')
+
+    print(f"Atomiq - {codePath}\n")
+
+    final = interpreter.transform(tree)
+
+    endTime = time.perf_counter()
+    totalTime = endTime - startTime
+    print(f'\nExecuted in: {totalTime:.2f} seconds.')
+    
+    return
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
