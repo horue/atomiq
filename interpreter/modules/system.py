9220ce463404bc625264ad0f546ca8026d62d013
Jorge <jorgejapao00@gmail.com>
2025-11-03 18:36:59 -0300

    function to save username on a file added

--- interpreter/modules/system.py
+++ interpreter/modules/system.py
@@ -1,5 +1,19 @@
+import os
+
 class System():
     @staticmethod
     def pause():
         print("Press any key to continue...")
         input()
+
+    @staticmethod
+    def getUsername():
+        username = os.getlogin()
+        return username
+    
+    @staticmethod
+    def saveUsername(fileName, mode):
+        username = System.getUsername().capitalize()
+        f = open(fileName, mode)
+        f.write(username)
+
