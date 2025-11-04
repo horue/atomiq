eaedcc62ce5b35b775ae55ed8757a0e165f5dbfd
Jorge <jorgejapao00@gmail.com>
2025-11-04 16:58:38 -0300

    chat function added, with file creation for memory

--- interpreter/modules/ollama.py
+++ interpreter/modules/ollama.py
@@ -17,6 +17,35 @@ class Ollama():
         else:
             print(finalResponse)
 
+    def chat(prompt, chatFile, show):
+        f = open(f'{chatFile}.memo', "a")
+        f.write(f'\nUser: {prompt}')
+        chatHistory = open(f'{chatFile}.memo', "r").read()
+        url = "http://localhost:11434/api/generate"
+        content = {"model": "mistral", "prompt": f'Use the chat history as a base for the conversation: {chatHistory} New prompt: {prompt}', "stream":False}
+        requestResponse = requests.post(url, json=content).json()
+        data = requestResponse
+
+
+        finalResponse=data["response"]
+        f.write(f"\nOllama model: {finalResponse}")
+        if show == "False":
+            pass
+        else:
+            print(finalResponse)
+
+    def clearMemo(target):
+        f = open(target, "w")
+        f.write(f'')
+
+
+
 
 if __name__ == "__main__":
-    Ollama.generate('Escreva duas letras aleatórias.', "True")
+    option = 3
+    if option == 1:
+        Ollama.generate('Escreva duas letras aleatórias.', "True")
+    if option == 2:
+        Ollama.chat('Qual o mais BARATO para se viver?', "chat1.memo", "True")
+    if option == 3:
+        Ollama.clearMemo("chat1.memo")
