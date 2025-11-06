1da43543ad325eefec758c030a4d33897a0458ef
Jorge <jorgejapao00@gmail.com>
2025-11-06 20:09:29 -0300

    variables added

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -2,15 +2,32 @@ from lark import Transformer
 from interpreter.stdlib import *
 
 
+class Utils():
+    def resulveValue(value):
+        from lark.lexer import Token
+        if isinstance(value, Token):
+            if value.type == "INT":
+                return int(value.value)
+            elif value.type == "FLOAT":
+                return float(value.value)
+            elif value.type == "ESCAPED_STRING":
+                return value.value.strip('"').strip("'")
+            else:
+                return value.value
+        return value
+
+
 class AtomiqInterpreter(Transformer):
     def __init__(self):
         self.scope={}
+        self.codeVars={}
         self.native_classes=STD
 
     def declaration_statement(self, items):
         name = items[1].value
         value = items[3] 
         self.scope[name] = value
+        self.codeVars[name] = value
         return None
     
     def args(self, items):
@@ -26,9 +43,15 @@ class AtomiqInterpreter(Transformer):
             method_name = items[1].value
             args = items[3]
 
+            if args[0] in self.scope:
+                resolved = Utils.resulveValue(self.scope[args[0]])
+            else:
+                resolved = Utils.resulveValue(args[0])
+
+
             metodo = self.native_classes[class_name][method_name] 
 
-            return metodo(*args)
+            return metodo(resolved)
 
     def value(self, items):
             item = items[0]
